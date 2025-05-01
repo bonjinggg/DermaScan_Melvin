e0fc45493a8e0f0aebc711738739301e99c6fc4a
JRynSalazar <daisuke.ryn05@gmail.com>
2025-05-01 09:45:38 +0800

    added - terms and Conditions, Privacy Policy

--- app/src/main/java/com/example/dermascanai/Login.kt
+++ app/src/main/java/com/example/dermascanai/Login.kt
@@ -18,7 +18,7 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.R
+
 
 
 class Login : AppCompatActivity() {
